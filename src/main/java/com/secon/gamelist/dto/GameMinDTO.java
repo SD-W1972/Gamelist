ad244855d9287ba21279726b50c89136648b8c99
SD-W1972 <pernalongaesnobe@gmail.com>
2024-11-14 09:05:11 -0300

    findByList endpoint

--- src/main/java/com/secon/gamelist/dto/GameMinDTO.java
+++ src/main/java/com/secon/gamelist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.secon.gamelist.dto;
 
 import com.secon.gamelist.entities.Game;
+import com.secon.gamelist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -23,6 +24,16 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+
+	
 	public Long getId() {
 		return id;
 	}
